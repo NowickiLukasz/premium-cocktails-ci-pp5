1edac70bdada436af92976d4321164d238f85ec7
NowickiLukasz <nowicki.lukasz93@gmail.com>
2022-08-21 14:49:32 +0000

    Initial commit

---
+++ premium_cocktails/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for premium_cocktails project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'premium_cocktails.settings')
+
+application = get_asgi_application()
