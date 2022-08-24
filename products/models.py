70f28e14f19e2a468305d5889297b979fabfc0db
NowickiLukasz <nowicki.lukasz93@gmail.com>
2022-08-24 17:12:42 +0000

    Added products app, models, and added products

---
+++ products/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name_plural = "categories"
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=150)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+    decription = models.TextField()
+    abv = models.DecimalField(max_digits=3, decimal_places=1)
+
+    def __str__(self):
+        return self.name
