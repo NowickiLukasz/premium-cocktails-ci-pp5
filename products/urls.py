026369c97a587b4170df6b32af45239eff91a583
NowickiLukasz <nowicki.lukasz93@gmail.com>
2022-09-13 15:23:37 +0000

    Added functionality to edit products

--- products/urls.py
+++ products/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.all_products, name='products'),
     path('cocktails/', views.all_cocktails, name='all_cocktails'),
     path('books/', views.all_books, name='all_books'),
-    path('products/<product_id>', views.product_details_page, name='product_details_page'),
+    path('<int:product_id>', views.product_details_page, name='product_details_page'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>', views.edit_product, name='edit_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -118,3 +118,27 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated item!')
+            return redirect(reverse('product_details_page', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update item. Please ensure the form is valid.')
+
+    form = ProductForm(instance=product)
+    messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
