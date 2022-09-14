57dbf6d9d581f887d0554d6486fa9f548cac4a1f
NowickiLukasz <nowicki.lukasz93@gmail.com>
2022-09-14 14:23:13 +0000

    Remove secret jek and set debug

--- premium_cocktails/settings.py
+++ premium_cocktails/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@9o-zofwuyvo$ki6bm7dsz^afenutz2f66&hm*@j(tbg9)@&xk'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -137,8 +137,6 @@ else:
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-
-
 
 
 # Password validation
