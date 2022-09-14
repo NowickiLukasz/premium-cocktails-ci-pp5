ee57d4253ae084ae982dc7049745f7fcf05d70de
NowickiLukasz <nowicki.lukasz93@gmail.com>
2022-09-14 12:57:47 +0000

    changes database environment variable in setting.py

--- premium_cocktails/settings.py
+++ premium_cocktails/settings.py
@@ -128,7 +128,7 @@ WSGI_APPLICATION = 'premium_cocktails.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse('DATABASE_URL')
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
