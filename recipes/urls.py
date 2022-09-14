fcbd609c1e5cd8b27cff89dd0da207dcf5b95c5d
NowickiLukasz <nowicki.lukasz93@gmail.com>
2022-09-14 21:57:58 +0000

    Added recipe details view and template

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_recipes, name='all_recipes'),
+    path('recipe_details/<int:recipe_id>', views.recipe_details, name='recipe_details'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Recipe
 
 
 def all_recipes(request):
     """
     Allows to view recipes all recipes
     """
+    recipes = Recipe.objects.all()
+
+    context = {
+        'recipes': recipes,
+
+    }
 
     template = 'recipes/recipes.html'
-    return render(request, template)
+    return render(request, template, context)
+
+
+def recipe_details(request, recipe_id):
+    """Allows to view a specific recipe"""
+
+    recipe = get_object_or_404(Recipe, id=recipe_id)
+
+    context = {
+        'recipe': recipe,
+    }
+
+    template = 'recipes/recipe_details.html'
+    return render(request, template, context)
