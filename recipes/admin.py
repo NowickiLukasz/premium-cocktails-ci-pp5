80ca3581494fbc11ddf31198b48122208eb4f536
NowickiLukasz <nowicki.lukasz93@gmail.com>
2022-09-14 20:31:03 +0000

    Added model and admin to recipes

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Recipe, RecipeIngredient
 
 # Register your models here.
+
+
+class RecipeIngredientInline(admin.StackedInline):
+    model = RecipeIngredient
+    extra = 0
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngredientInline]
+    list_display = ['user', 'name']
+    readonly_fields = ['user', 'date_created']
+
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(RecipeIngredient)
+
