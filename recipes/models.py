73a6d76a700caccf82eb3614b4c9f2929ea0260b
NowickiLukasz <nowicki.lukasz93@gmail.com>
2022-09-15 12:34:40 +0000

    Added functionality to iterate thorugh ingredients in cocktail details page

--- recipes/models.py
+++ recipes/models.py
@@ -7,7 +7,11 @@ class Recipe(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=254, blank=False, null=False)
     method = models.TextField()
+    image = models.ImageField(null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
 
 
 class RecipeIngredient(models.Model):
--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 
-from .models import Recipe
+from .models import Recipe, RecipeIngredient
 
 
 def all_recipes(request):
@@ -22,9 +22,13 @@ def recipe_details(request, recipe_id):
     """Allows to view a specific recipe"""
 
     recipe = get_object_or_404(Recipe, id=recipe_id)
+    recipe_ingredients = RecipeIngredient.objects.all()
 
+    # recipe_ingredient = get_object_or_404(RecipeIngredient)
+ 
     context = {
         'recipe': recipe,
+        'recipe_ingredients': recipe_ingredients,
     }
 
     template = 'recipes/recipe_details.html'
