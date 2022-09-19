8a4480b62ca47109bd23cdbe1a9417f3cebacf29
NowickiLukasz <nowicki.lukasz93@gmail.com>
2022-09-19 21:36:49 +0000

    Added delete functionlaity for rercepies

--- recipes/urls.py
+++ recipes/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('add_recipe/', views.add_recipe, name='add_recipe'),
     path('add_recipe_ingredient/<int:recipe_id>', views.add_recipe_ingredient, name='add_recipe_ingredient'),
     path('edit_recipe/<int:recipe_id>', views.edit_recipe, name='edit_recipe'),
+    path('delete_recipe/<int:recipe_id>', views.delete_recipe, name='delete_recipe'),
 ]
