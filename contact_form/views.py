dff393d9b741128ce41e6838a05829ae45f3c772
NowickiLukasz <nowicki.lukasz93@gmail.com>
2022-09-20 19:57:15 +0000

    Added template details and form submition for contact us page

--- contact_form/views.py
+++ contact_form/views.py
@@ -1,16 +1,28 @@
-from django.shortcuts import render
-# from .forms import ContactUsForm
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import ContactUsForm
 
 
 def contact_us(request):
     """Allows users to contact site owner"""
 
-    # contact_form = ContactUsForm()
+    contact_form = ContactUsForm()
+
+    if request.method == 'POST':
+        contact_form = ContactUsForm(request.POST, request.FILES)
+        if contact_form.is_valid():
+            contact_form.save()
+            messages.success(request, 'Successfully added item!')
+            return redirect(reverse('home'))
+        else:
+            messages.error(request, 'Failed to add item. Please ensure the form is valid.')
+    else:
+        contact_form = ContactUsForm()
 
     template = 'contact/contact_us.html'
-    # context = {
-    #     'contact_form': contact_form,
-    # }
+    context = {
+        'contact_form': contact_form,
+    }
 
-    # return render(request, template, context)
-    return render(request, template)
+    return render(request, template, context)
+    # return render(request, template)
