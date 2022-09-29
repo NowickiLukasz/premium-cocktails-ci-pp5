1bf465828f3bb286a7f27fecb6dad838bc0ba4d1
NowickiLukasz <nowicki.lukasz93@gmail.com>
2022-09-29 12:36:29 +0000

    Added styling to readme page

--- products/views.py
+++ products/views.py
@@ -20,7 +20,6 @@ def all_products(request):
     direction = None
     category = None
 
-
     if request.GET:
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
@@ -28,7 +27,7 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-        
+
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
